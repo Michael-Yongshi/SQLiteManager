38b3ef7d43c29902a5be1b75500258ca6f513407
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-09-08 21:59:36 +0200

    added dependency on SQLite3

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Michael-Yongshi/SQLiteManager",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'sqlite3',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
