a4867086760afc564b66e9f328d4053ba9e81de5
Yongshi <4registration@outlook.com>
2023-04-18 12:43:20 +0200

    fixed print and removed unused methods

--- sqlitemanager/record.py
+++ sqlitemanager/record.py
@@ -14,8 +14,8 @@ class Record(object):
     def print(self):
 
         print_string = ""
-        for key, value in self.dict:
-            print_string += f"{key}: {value}, "
+        for column_name, value in self.dictionary.items():
+            print_string += f"{column_name}: {value}, "
         print_string = print_string[:-2]
 
         print(print_string)
@@ -29,32 +29,32 @@ class Record(object):
 
         return column_names
 
-    def setvaluepairs(self, column_names):
-        self.valuepairs = []
-        for index, name in enumerate(column_names[1:]):
-            valuepair = [name, self.recordarray[1:][index]]
-            self.valuepairs += [valuepair]
-        # print(f"set valuepairs {self.valuepairs}")
+    # def setvaluepairs(self, column_names):
+    #     self.valuepairs = []
+    #     for index, name in enumerate(column_names[1:]):
+    #         valuepair = [name, self.recordarray[1:][index]]
+    #         self.valuepairs += [valuepair]
+    #     # print(f"set valuepairs {self.valuepairs}")
 
-    def setrecorddict(self, column_names):
-        self.recorddict = {}
-        for index, name in enumerate(column_names[1:]):
-            self.recorddict.update({name: self.recordarray[1:][index]})
-        # print(f"set recorddict {self.recorddict}")
+    # def setrecorddict(self, column_names):
+    #     self.recorddict = {}
+    #     for index, name in enumerate(column_names[1:]):
+    #         self.recorddict.update({name: self.recordarray[1:][index]})
+    #     # print(f"set recorddict {self.recorddict}")
 
-    def get_column_value(self, column_name):
-        """
-        method to easily retrieve a value for a specific column
-        """
+    # def get_column_value(self, column_name):
+    #     """
+    #     method to easily retrieve a value for a specific column
+    #     """
 
-        # loop over all records valuepairs
-        for valuepair in self.valuepairs:
+    #     # loop over all records valuepairs
+    #     for valuepair in self.valuepairs:
             
-            # if column name is found return the value
-            if valuepair[0] == column_name:
+    #         # if column name is found return the value
+    #         if valuepair[0] == column_name:
 
-                column_value = valuepair[1]
-                return column_value
+    #             column_value = valuepair[1]
+    #             return column_value
 
-        print("Column not found")
-        return "Column not found"
+    #     print("Column not found")
+    #     return "Column not found"
